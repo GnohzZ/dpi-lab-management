18964c76ed1ab0645356bcedde2bf62dbdb8f167
GnohzZ <z100965@163.com>
2020-07-24 22:51:43 +0800

    edifice

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,126 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="flex-grow-1 flex-shrink-0"
+      hide-overlay
+      flat
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item class="px-2">
+              <v-list-item-avatar>
+                <v-img></v-img>
+              </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title">Hello! {{username}}</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+          mandatory
+        >
+
+          <v-list-item
+            @click="toHome"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="toSearch">
+            <v-list-item-icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Search</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="toAccount">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="toAbout">
+            <v-list-item-icon>
+              <v-icon>mdi-information</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      flat
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-app-bar-nav-icon>
+      <v-toolbar-title>DPI Lab</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+  <v-main>
+    <router-view> </router-view>
+  </v-main>
+
+  <v-footer
+    app
+  >
+    <span>Powered by Vue</span>
+    <v-spacer></v-spacer>
+    <span>&copy; {{ new Date().getFullYear() }}</span>
+  </v-footer>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
+
   },
 
   data: () => ({
-    //
-  })
+    drawer: false,
+    username: ''
+  }),
+
+  computed: {
+
+  },
+
+  methods: {
+    toHome () {
+      this.$router.push('/home')
+      console.log(this.$route.path)
+    },
+    toSearch () {
+      this.$router.push('/search')
+    },
+    toAccount () {
+      this.$router.push('/account/' + this.username)
+    },
+    toAbout () {
+      this.$router.push('/about')
+    }
+  }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,11 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '',
+    redirect: 'Home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
@@ -17,11 +21,28 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/account/:username',
+    name: 'Account',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Account.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue')
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
