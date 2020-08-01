23e23105a880b00b373f9d6c5c5a803ba82874f8
GnohzZ <z100965@163.com>
2020-08-01 17:39:47 +0800

    add notices on home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <v-container
     class="flex-grow-1 flex-shrink-0"
     fluid
+    v-resize = "onResize"
   >
     <v-row
       align="start"
@@ -17,64 +18,65 @@
             class = "py-1"
           >
             <v-card-title
-              class = "pt-0"
+              class = "pt-3"
             >
-              Nutrition
-              <v-spacer></v-spacer>
-              <v-text-field
-                v-model="search"
-                append-icon="mdi-magnify"
-                label="Search"
-                single-line
-                hide-details
-              ></v-text-field>
+              Notice
             </v-card-title>
-            <v-data-table
-              :height="maxHeight"
-              fixed-header
-              :loading = "tableLoading"
-              :headers="headers"
-              :items="desserts"
-              :search="search"
-              :footer-props="tFooterProp"
-              :page.sync="page"
-              @page-count="pageCount = $event"
-              :items-per-page="itemsPerPage"
-              hide-default-footer
-              v-resize = "onResize"
-            ></v-data-table>
-            <v-divider></v-divider>
-            <v-row
-              align="center"
-              justify="center"
+            <v-card
+              flat
+              :max-height = "maxHeight"
+              class = "overflow-y-auto"
             >
-              <v-spacer class="hidden-sm-and-down"></v-spacer>
-              <v-select
-                v-model = "itemsPerPage"
-                :items="itemNumberPerPage"
-                label="Items per page"
-                dense
-                class="mt-4 mb-n4 ml-6 mr-6"
-              ></v-select>
-              <v-pagination
-                v-model="page"
-                :length="pageCount"
-                :total-visible="3"
-                class = "pb-0 mb-n1 mt-0 mr-3"
-              ></v-pagination>
-            </v-row>
+              <v-expansion-panels
+                focusable
+                :value = "selectedPanel"
+              >
+                <v-expansion-panel
+                  v-for="(notice,i) in sortedNotices"
+                  :key="i"
+                >
+                  <v-expansion-panel-header>
+                    <v-row class = "py-0">
+                      <v-col class = "py-1" v-if = "breakPoint !== 'xs'">
+                        <!-- <v-card
+                          width = "190"
+                          flat
+                        > -->
+                          <span class="font-weight-light">{{notice.publishTime}}</span>
+                          <span class="font-italic" v-if = "notice.toTop"> [ sticky ] </span>
+                        <!-- </v-card> -->
+                      </v-col>
+                      <v-col class = "py-1 mx-1" align = "start" md = "9" sm = "7">
+                        <span class="font-weight-black" :class="{ 'primary--text': notice.importance }" >
+                          {{notice.title}}
+                        </span>
+                      </v-col>
+                    </v-row>
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content class = "mt-4">
+                    <span class="font-weight-light" v-if = "breakPoint === 'xs'">{{notice.publishTime}}</span>
+                    <p>{{notice.content}}</p>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-card>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
+<style>
+
+</style>
+
 <script>
 export default {
   data () {
     return {
+      selectedPanel: 0, // 若要启用multiple属性，将此处改为数组
+      breakPoint: '',
       windowHeight: 500,
-      tableLoading: false,
       page: 1,
       itemsPerPage: 10,
       itemNumberPerPage: [5, 10, 15, 20],
@@ -82,131 +84,62 @@ export default {
       tFooterProp: {
         'items-per-page-text': 'Per page'
       },
-      headers: [
+      notices: [
         {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
-      ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
+          title: '重要置顶',
+          publishTime: '2020-08-01 14:14',
+          content: '今儿👴在做Home页',
+          importance: 1,
+          toTop: true
         },
         {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
+          title: '重要不置顶试一试标题长度的影响吧吧吧吧吧吧',
+          publishTime: '2020-08-01 14:20',
+          content: '今儿👴在做Home页',
+          importance: 1,
+          toTop: false
         },
         {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
+          title: '重要不置顶',
+          publishTime: '2020-07-01 14:14',
+          content: '下个月👴在做Home页',
+          importance: 1,
+          toTop: false
         },
         {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
+          title: '不重要置顶',
+          publishTime: '2020-02-01 14:14',
+          content: '半年后👴在做Home页',
+          importance: 0,
+          toTop: true
         },
         {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
+          title: '不重要不置顶',
+          publishTime: '2020-08-01 14:14',
+          content: '今儿👴在做Home页',
+          importance: 0,
+          toTop: false
         },
         {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
+          title: '不重要不置顶',
+          publishTime: '2020-08-01 14:14',
+          content: '今儿👴在做Home页',
+          importance: 0,
+          toTop: false
         },
         {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
+          title: '不重要不置顶',
+          publishTime: '2020-08-01 14:14',
+          content: '今儿👴在做Home页',
+          importance: 0,
+          toTop: false
         },
         {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
+          title: '来个老一点的',
+          publishTime: '2020-01-01 14:14',
+          content: '半年以后👴在做Home页',
+          importance: 0,
+          toTop: false
         }
       ]
     }
@@ -215,24 +148,54 @@ export default {
   methods: {
     onResize: function () {
       this.windowHeight = window.innerHeight
+      this.breakPoint = this.$vuetify.breakpoint.name
     }
   },
 
   computed: {
     maxHeight: function () {
-      const mheight = this.windowHeight - 265
-      if (mheight > 48 * (this.itemsPerPage + 1)) return 48 * (this.itemsPerPage + 1)
-      else return this.windowHeight - 265
+      return this.windowHeight - 200
     },
     pageCount: {
       get () {
         return Math.ceil(this.desserts.length / this.itemsPerPage)
       },
       set () {}
+    },
+
+    sortedNotices: function () {
+      let sn = this.notices
+      sn.sort((a, b) => {
+        if (a.toTop !== b.toTop) {
+          return b.toTop - a.toTop
+        } else if (b.publishTime.split(' ').join('').split('-').join('').split(':').join('') !== a.publishTime.split(' ').join('').split('-').join('').split(':').join('')) {
+          return b.publishTime.split(' ').join('').split('-').join('').split(':').join('') - a.publishTime.split(' ').join('').split('-').join('').split(':').join('')
+        } else return b.importance - a.importance
+      })
+
+      // 筛除六个月前的notice和超过7条的notice
+      const today = new Date()
+      const todayString = today.getFullYear() + (today.getMonth() < 9 ? ('0' + (today.getMonth() + 1)) : (today.getMonth() + 1))
+      for (let i = 1; i < sn.length; i++) {
+        if (((todayString - sn[i].publishTime.split(' ').shift().split('-').slice(0, 2).join('') > 6) || (i > 6)) && !sn[i].toTop) {
+          sn = sn.slice(0, i)
+          break
+        }
+      }
+      return sn
     }
   },
   created () {
     this.onResize()
+    // 若expansion panel改为multiple属性，则取消此处注释可实现自动打开所有置顶notice
+    // let count = 0
+    // for (let i = 0; i < this.notices.length; i++) {
+    //   if (this.notices[i].toTop) {
+    //     this.selectedPanel.push(count)
+    //     count++
+    //   }
+    // }
+    // if (count === 0) this.selectedPanel.push(0)
   }
 }
 </script>
